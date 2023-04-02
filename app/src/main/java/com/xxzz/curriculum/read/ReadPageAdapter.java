83a6eaa163178aa4e39ab0a281544ce24427fef7
zhang <1274556865@qq.com>
2023-04-02 10:15:53 +0800

    update

--- app/src/main/java/com/xxzz/curriculum/read/ReadPageAdapter.java
+++ app/src/main/java/com/xxzz/curriculum/read/ReadPageAdapter.java
@@ -19,20 +19,19 @@ public class ReadPageAdapter extends PagerAdapter {
         return views.size();
     }
 
-    //滑动切换的时候销毁当前的组件
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position,
                             @NonNull Object object) {
         ((ViewPager) container).removeView(views.get(position));
     }
 
-    //将当前视图添加到container中并返回当前View视图
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         ((ViewPager) container).addView(views.get(position));
         return views.get(position);
     }
+
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
         return view == object;
