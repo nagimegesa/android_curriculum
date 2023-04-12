7a300c20a1ac8910d44efd41442dc73ef3a3ab65
mysuintro <2807936041@qq.com>
2023-04-12 20:49:06 +0800

    增加了背景音乐的设置

--- app/src/main/java/com/xxzz/curriculum/index/BackgroundMusic.java
+++ app/src/main/java/com/xxzz/curriculum/index/BackgroundMusic.java
@@ -1,25 +1,46 @@
 package com.xxzz.curriculum.index;
 
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
 
-public class BackgroundMusic {
+import androidx.annotation.Nullable;
 
-    private static MediaPlayer mp = null;
+public class BackgroundMusic extends Service {
+    // TODO: add local music
+    private static MediaPlayer mediaPlayer = null;
 
-    public static void play(Context context, int resource) {
+    public void play(Context context, int resource) {
         stop(context);
-        mp = MediaPlayer.create(context, resource);
-        mp.setLooping(true);
-        mp.start();
+        mediaPlayer = MediaPlayer.create(context, resource);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
     }
 
-    public static void stop(Context context) {
-        if (mp != null) {
-            mp.stop();
-            mp.release();
-            mp = null;
+    public void stop(Context context) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
         }
     }
 
+    @Override
+    public void onDestroy() {
+        //stop();
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new PlayBinder();
+    }
+
+    public class PlayBinder extends Binder {
+
+    }
 }
