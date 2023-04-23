495d0298698342186a673629279e24ed1153a210
xxy <2590370287@qq.com>
2023-04-23 16:31:36 +0800

    临时提交

--- app/src/main/java/com/xxzz/curriculum/index/IndexFragment.java
+++ app/src/main/java/com/xxzz/curriculum/index/IndexFragment.java
@@ -1,8 +1,12 @@
 package com.xxzz.curriculum.index;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.icu.text.IDNA;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
@@ -20,21 +24,26 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class IndexFragment extends Fragment{
-    public  DragGridView gridview;
+public class IndexFragment extends Fragment {
+    public DragGridView gridview;
     private IndexBookAdapter adapter;
-    private static List<BooKInfo> list=new ArrayList<>();
-    private List<String> bookNameList=new ArrayList<String>();
+    private static List<BooKInfo> list = new ArrayList<>();
+    private List<String> bookNameList = new ArrayList<String>();
     private static IndexFragment fragment;
 
-    public IndexFragment()  {
+    public IndexFragment() {
 
     }
+
     /**
      * 单例模式, 写的时候只能用这个得到 IndexFragment 的实例, 不要直接调用 new
      *
@@ -45,8 +54,9 @@ public class IndexFragment extends Fragment{
             fragment = new IndexFragment();
         return fragment;
     }
+
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
@@ -54,19 +64,19 @@ public class IndexFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_index,container,false);
-        gridview= (DragGridView) view.findViewById(R.id.list);
+        View view = inflater.inflate(R.layout.fragment_index, container, false);
+        gridview = (DragGridView) view.findViewById(R.id.list);
 //        ((Button)view.findViewById(R.id.index_add_button)).setOnClickListener(this::add);
 //        ((Button)view.findViewById(R.id.index_del_button)).setOnClickListener(this::del);
         adapter = new IndexBookAdapter(getActivity(), list);
         gridview.setAdapter(adapter);
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 //                Bundle bundle = new Bundle();
 //                bundle.putString("book_name", adapter.datas.get(position));
                 Intent intent = new Intent();
-                intent.putExtra("book_name",bookNameList.get(position));
+                intent.putExtra("book_name", bookNameList.get(position));
                 intent.setClass(getContext(), ReadActivity.class);
                 startActivity(intent);
 //                switch (position) {
@@ -80,48 +90,65 @@ public class IndexFragment extends Fragment{
             @Override
             public void onChange(int from, int to) {
                 BooKInfo temp = list.get(from);
-                if(from < to){
-                    for(int i=from; i<to; i++){
-                        Collections.swap(list, i, i+1);
+                if (from < to) {
+                    for (int i = from; i < to; i++) {
+                        Collections.swap(list, i, i + 1);
                     }
-                }else if(from > to){
-                    for(int i=from; i>to; i--){
-                        Collections.swap(list, i, i-1);
+                } else if (from > to) {
+                    for (int i = from; i > to; i--) {
+                        Collections.swap(list, i, i - 1);
                     }
                 }
                 list.set(to, temp);
                 adapter.notifyDataSetChanged();
             }
         });
+        try {
+            creatBookInfoFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         readBookInfoFromFile();
         return view;
     }
 
-    private void readBookInfoFromFile(){
+    private void creatBookInfoFile() throws IOException {
+        String path = getActivity().getFilesDir() + "/cover";
+        File file = new File(path, "text.json");
+        if (!file.exists())
+            file.createNewFile();
+    }
+
+
+    private void readBookInfoFromFile() {
 //        this.list = new ArrayList<>();
-//        this.bookNameList = new ArrayList<String>();
         String res = null;
         try {
             FragmentActivity activity = getActivity();
             res = Utils.readAllFile(activity.getFilesDir().toPath().resolve("cover/test.json").toFile().toPath());
             JSONObject context = new JSONObject(res);
             JSONArray array = context.getJSONArray("cover");
-            for (int i=0;i<array.length();i++){
+            for (int i = 0; i < array.length(); i++) {
                 JSONObject object = array.getJSONObject(i);
-                    if(!bookNameList.contains(object.getString("book_name"))) {
-                        bookNameList.add(object.getString("book_name"));
-                        BooKInfo bookinfo = new BooKInfo(object.getString("book_name"), object.getString("cover_path"), object.getString("last_read_time"));
-                        list.add(bookinfo);
-                    }
+                if (!bookNameList.contains(object.getString("book_name"))) {
+                    bookNameList.add(object.getString("book_name"));
+                    BooKInfo bookinfo = new BooKInfo(object.getString("book_name"), object.getString("cover_path"), object.getString("last_read_time"));
+                    list.add(bookinfo);
+                }
             }
         } catch (IOException | JSONException e) {
             throw new RuntimeException(e);
         }
     }
+
     public List<BooKInfo> getList() {
         return list;
     }
-    public IndexBookAdapter getAdapter() {return adapter;}
+
+    public IndexBookAdapter getAdapter() {
+        return adapter;
+    }
+
     public List<String> getBookNameList() {
         return bookNameList;
     }
